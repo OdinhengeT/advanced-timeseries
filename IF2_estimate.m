80acf3d5402c53712ec095b04e27bdcc685829d1
Aron Paulsson <aronpaulsson99@gmail.com>
2024-01-02 14:31:32 +0100

    made changes to algorithms and added new algorithms

--- IF2_estimate.m
+++ IF2_estimate.m
@@ -1,32 +1,38 @@
-function [estimated_states] = IF2_estimate(data, ObservationDensity, TransitionDensity, numParticles)
-if nargin < 4
-    error("Error. Too few input arguments")
-end
+function [estimatedStates, estimatedParameters] = IF2_estimate(data, transitionFunction, observationFunction, numStates, numParameters)
+M = 100;
+N = length(data);
+J = 10000;
 
-% Initialize particles
-particles = randn(numParticles);                                            % Initialize state particles randomly
-mean_a = 0;
-T = length(data);
-estimated_states = zeros(T,numParticles(2));
+estimatedStates = zeros(N,M,numStates);
+estimatedParameters = zeros(M,numParameters);
 
-% SISR Algorithm
-for t = 1:T
-    % Mutation
-    particles = TransitionDensity(particles, t);
-    if t > 30                                                               % Use averaging after burn in pereiod
-        mean_a = mean_a + (particles(:,2) - mean_a)/(t-30);
-    end
+param_particles = zeros(J, numParameters);
+states_particles = zeros(J, numStates);
+Sigma = eye(numParameters);
 
-    % Update weights
-    weights = ObservationDensity(data(t), particles);                       % Measurement likelihood
+h = @(theta_F, sigma_m) normrnd(theta_F, sigma_m*Sigma);
+f_init = @(x_0, theta_F_0, sigma) normrnd(transitionFunction(x_0, theta_F_0), sigma);
+f_tran = @(x_F,theta_P, sigma) normrnd(transitionFunction(x_F, theta_P), sigma);
+f_obs_pdf = @(y,x_P,theta_P,sigma) normpdf(y,observationFunction(x_P, theta_P), sigma);
+
+for m = 1:M
+    m
+    sigma_m = 1/(m);
+    param_particles_F = h(param_particles, sigma_m);
+    states_particles_F = f_init(states_particles, param_particles_F, 1);
     
-    % Selection and Resampling
-    resampledIndices = randsample(1:numParticles(1), numParticles(1), true, weights);
-    particles = particles(resampledIndices,:);
-
-    % State estimation
-    weights = weights / sum(weights);                                       % Normalize weights
-    estimated_states(t,:) = sum(weights .* particles);                      % Simplified since weights are already normalized
+    for n = 1:N
+        param_particles_P = h(param_particles_F, sigma_m);
+        states_particles_P = f_tran(states_particles_F, param_particles_P, 1);
+        weights = f_obs_pdf(data(n), states_particles_P, param_particles_P, 1);
+        weights = weights / sum(weights);
+        resampledIndices = randsample(1:J, J, true, weights);
+        param_particles_F = param_particles_P(resampledIndices);
+        states_particles_F = states_particles_P(resampledIndices);
+        estimatedStates(n,m,:) = sum(weights .* states_particles_F);
+    end
+    param_particles = param_particles_F;
+    state_particles = states_particles_F;
+    estimatedParameters(m,:) = mean(param_particles);
 end
 end
-
